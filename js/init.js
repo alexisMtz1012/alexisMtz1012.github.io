d6d6e0a50535b2199997aea4a05ce8564c4c9512
alexisMtz1012 <82429994+alexisMtz1012@users.noreply.github.com>
2021-04-13 21:14:42 -0500

    commit corecciones

--- js/init.js
+++ js/init.js
@@ -1,10 +1,10 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+  apiKey: "AIzaSyA4QfaI1y3owQPtEUVwkPlMSjFpuLPygCY",
+  authDomain: "alexisbd-8f153.firebaseapp.com",
+  projectId: "alexisbd-8f153",
+  storageBucket: "alexisbd-8f153.appspot.com",
+  messagingSenderId: "305276784968",
+  appId: "1:305276784968:web:259b63d67dd9f536f9237d",
+  measurementId: "G-NDPMDHKK7X"
 });
